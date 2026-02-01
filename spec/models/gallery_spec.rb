5ec5bb1d04d6fb61639c2a85f5ed084926b0ca73
Stephen McCullough <me@swm.cc>
2026-02-01 23:32:15 +0000

    ✅ Add model specs for User, Gallery, and Upload
    Test associations, validations, enums, scopes, and callbacks
    using shoulda-matchers. Replaces pending placeholder tests.

--- spec/models/gallery_spec.rb
+++ spec/models/gallery_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Gallery, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to have_many(:uploads).dependent(:destroy) }
+  end
+
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_presence_of(:user) }
+  end
+
+  describe "scopes" do
+    describe ".recent" do
+      it "returns galleries ordered by created_at desc" do
+        user = create(:user)
+        old_gallery = create(:gallery, user: user, created_at: 2.days.ago)
+        new_gallery = create(:gallery, user: user, created_at: 1.day.ago)
+
+        expect(Gallery.recent).to eq([new_gallery, old_gallery])
+      end
+    end
+  end
 end
