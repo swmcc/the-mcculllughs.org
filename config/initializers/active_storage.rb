02504c0ce3f7ee783992f594d09ff056deeddcd9
Stephen McCullough <me@swm.cc>
2026-02-21 02:14:13 +0000

    🐛 Fix public photo images requiring authentication
    Skip Devise authentication for Active Storage blob/representation
    controllers so public shared photos can load images

---
+++ config/initializers/active_storage.rb
@@ -0,0 +1,8 @@
+# Make Active Storage blobs publicly accessible
+# This allows public photo sharing to work without authentication
+Rails.application.config.to_prepare do
+  ActiveStorage::Blobs::RedirectController.skip_before_action :authenticate_user!, raise: false
+  ActiveStorage::Blobs::ProxyController.skip_before_action :authenticate_user!, raise: false
+  ActiveStorage::Representations::RedirectController.skip_before_action :authenticate_user!, raise: false
+  ActiveStorage::Representations::ProxyController.skip_before_action :authenticate_user!, raise: false
+end
