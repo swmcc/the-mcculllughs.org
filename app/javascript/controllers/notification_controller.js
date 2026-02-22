409a897f27eac4a416a34836e55f180437c92e43
Stephen McCullough <me@swm.cc>
2026-02-22 18:26:07 +0000

    🎨 Add cover photo button UI with Turbo Stream updates

---
+++ app/javascript/controllers/notification_controller.js
@@ -0,0 +1,26 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static values = {
+    timeout: { type: Number, default: 5000 }
+  }
+
+  connect() {
+    this.timeout = setTimeout(() => {
+      this.dismiss()
+    }, this.timeoutValue)
+  }
+
+  disconnect() {
+    if (this.timeout) {
+      clearTimeout(this.timeout)
+    }
+  }
+
+  dismiss() {
+    this.element.classList.add("opacity-0", "transition-opacity", "duration-300")
+    setTimeout(() => {
+      this.element.remove()
+    }, 300)
+  }
+}
